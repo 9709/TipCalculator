95c960b717717404a0cdf93241a539334759964b
Matthew Chan <mat_t4@hotmail.com>
2019-01-18 13:09:47 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Matthew Chan on 2019-01-18.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
